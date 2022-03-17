fd6f399757925ba2989845a5ccba42e9796b396c
Rakesh Baruah <rbaruah@gmail.com>
2022-03-17 11:52:43 -0500

    working todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Todo } from "./models/todo.model";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
@@ -7,8 +7,10 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   function addTodo(todo: string) {
-    setTodos([{ id: Math.random().toString(), text: todo }]);
-    console.log(todos);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: todo },
+    ]);
   }
 
   return (
